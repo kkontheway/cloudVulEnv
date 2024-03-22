26e8c14a8727fd1db2bdb1a96485ddfbf21c199b
kk <kk@kkdeMacBook-Pro.local>
2024-03-22 17:56:50 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
+struct Config {
+    Vuln: String,
+    DockerVersion: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let Vuln = args[1].clone();
+    let DockerVersion = args[2].clone();
+
+    Config {
+        Vuln,
+        DockerVersion,
+    }
 }
